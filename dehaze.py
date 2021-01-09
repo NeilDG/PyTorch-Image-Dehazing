c5691c8f604c640b914fbc18dd43c047bef9b7b1
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2021-01-09 15:05:08 +0800

    Changes to pycharm compatibility.

--- dehaze.py
+++ dehaze.py
@@ -15,9 +15,10 @@ from PIL import Image
 import glob
 
 
-def dehaze_image(image_path):
+def dehaze_image(results_path, image_path):
 
 	data_hazy = Image.open(image_path)
+	data_hazy = data_hazy.resize((512, 512))
 	data_hazy = (np.asarray(data_hazy)/255.0)
 
 	data_hazy = torch.from_numpy(data_hazy).float()
@@ -28,14 +29,33 @@ def dehaze_image(image_path):
 	dehaze_net.load_state_dict(torch.load('snapshots/dehazer.pth'))
 
 	clean_image = dehaze_net(data_hazy)
-	torchvision.utils.save_image(torch.cat((data_hazy, clean_image),0), "results/" + image_path.split("\\")[1])
+	#torchvision.utils.save_image(torch.cat((data_hazy, clean_image),0), "results/" + image_path.split("\\")[1])
+	torchvision.utils.save_image(clean_image, results_path + image_path.split("\\")[1])
 	
 
 if __name__ == '__main__':
 
-	test_list = glob.glob("test_images/*")
+	img_dir = "E:/Hazy Dataset Benchmark/O-HAZE/hazy/"
+	test_list = glob.glob(img_dir + "*.jpg")  # specify atmosphere intensityimg_dir = "D:/Datasets/OTS_BETA/haze/"
+	results_path = "results/O-Haze/"
 
 	for image in test_list:
+		dehaze_image(results_path, image)
+		print(image, "done!")
 
-		dehaze_image(image)
+	img_dir = "D:/Datasets/OTS_BETA/haze/"
+	test_list = glob.glob(
+		img_dir + "*0.95_0.2.jpg")  # specify atmosphere intensityimg_dir = "D:/Datasets/OTS_BETA/haze/"
+	results_path = "results/OTS-Beta/"
+
+	for image in test_list:
+		dehaze_image(results_path, image)
+		print(image, "done!")
+
+	img_dir = "E:/Hazy Dataset Benchmark/RESIDE-Unannotated/"
+	test_list = glob.glob(img_dir + "*.jpg")  # specify atmosphere intensityimg_dir = "D:/Datasets/OTS_BETA/haze/"
+	results_path = "results/RESIDE-3/"
+
+	for image in test_list:
+		dehaze_image(results_path, image)
 		print(image, "done!")
